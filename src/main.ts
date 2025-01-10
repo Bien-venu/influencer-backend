af3722b190d4b76767261b875366176296f1b86f
Bien-venu <iambienvenu@gmail.com>
2025-01-10 07:50:11 +0200

    update the main file

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import helmet from 'helmet';
+import * as compression from 'compression';
+import { VercelRequest, VercelResponse } from '@vercel/node';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.use(helmet());
+  app.use(compression());
+  app.enableCors();
+  app.useGlobalPipes(new ValidationPipe());
+
+  await app.init();
+
+  return app;
 }
-bootstrap();
+
+export const handler = async (req: VercelRequest, res: VercelResponse) => {
+  const app = await bootstrap();
+
+  app.getHttpAdapter().getInstance()(req, res);
+};
